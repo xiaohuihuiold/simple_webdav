597e64c4e0b26b8c3e704470ec8c8eb7450cbc6b
xiaohuihuiold <xiaohuihuiold@gmail.com>
2020-05-10 22:45:38 +0800

    去掉path参数

--- lib/src/webdav_client.dart
+++ lib/src/webdav_client.dart
@@ -12,7 +12,7 @@ class WebDAVClient {
   int port;
   Dio _dio;
 
-  bool get isAvailable => host != null && path != null && authenticate != null;
+  bool get isAvailable => host != null && authenticate != null;
 
   Map<String, dynamic> get headers => _dio.options.headers;
 
